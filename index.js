fc5bc864411d5187fac2bf10ae6b5f18b640da8e
Michael P <102651473+michmich16@users.noreply.github.com>
2025-02-03 13:49:12 +0100

    ddddd

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import  sequelize  from './config/sequelizeConfig.js';
+
+
+dotenv.config()
+console.log(process.env.PORT);
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (request, response) => {
+
+   response.send('velkommen til min side');
+})
+
+
+app.get('/about', (request, response) => {
+
+    response.send('velkommen til about side');
+ })
+
+ app.get('/contact', (req, res) => {
+    res.send('Dette er kontakt siden...');
+  });
+  
+
+app.listen(port, () =>{
+    console.log(`server kører på http//:localhost:${port}`);
+})
+
+app.get('/test', async (req, res) => {
+    try {
+      await sequelize.authenticate();
+      console.log('Der er forbindelse til databasen');
+      res.send('Der er forbindelse til databasen');
+    } catch (error) {
+      console.error('Fejl! Kunne ikke forbinde til databasen: ', error);
+      res.status(500).send('Fejl! Kunne ikke forbinde til databasen.');
+    }
+  });
+
+dotenv.config();
